928464ea3454232c19cca4eec144ac2016da5eca
igor5544 <igor5544@bk.ru>
2020-11-22 01:40:35 +0500

    Updates

--- static/js/9.69a7f101.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonplesson-1-kama"]=this["webpackJsonplesson-1-kama"]||[]).push([[9],{311:function(e,a,n){"use strict";n.r(a);var s=n(0),t=n.n(s);a.default=function(){return t.a.createElement("section",null,t.a.createElement("h2",{className:"main-title"},"News"))}}}]);
-//# sourceMappingURL=9.69a7f101.chunk.js.map
---
+++ static/js/9.9845b0c5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplesson-1-kama"]=this["webpackJsonplesson-1-kama"]||[]).push([[9],{312:function(e,a,n){"use strict";n.r(a);var s=n(0),t=n.n(s);a.default=function(){return t.a.createElement("section",null,t.a.createElement("h2",{className:"main-title"},"News"))}}}]);
+//# sourceMappingURL=9.9845b0c5.chunk.js.map
